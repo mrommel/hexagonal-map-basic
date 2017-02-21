295384054714f8b7bc5f4b4d588b73c92b07cf65
Michael Rommel <michael.rommel@mobility-media.de>
2017-02-21 16:07:08 +0100

    refactoring

--- src/Scenes/GameScene.swift
+++ src/Scenes/GameScene.swift
@@ -33,82 +33,82 @@ class GameScene: SKScene {
     var onFocusChanged: FocusChangedBlock?
     var onTileSelected: FocusChangedBlock?
     var cam: SKCameraNode!
-    
+
     let terrainTransitionManager: TerrainTransitionManager
-    
+
     let terrainView: SKSpriteNode
     //let layer2DHighlight: SKNode
-    
+
     // cursor handling
     var cursorNode: SKSpriteNode
     var cursorPoint: GridPoint?
-    
-    let tileSize = (width:64, height:64)
-    
+
+    let tileSize = (width: 64, height: 64)
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override init(size: CGSize) {
-        
+
         self.terrainView = SKSpriteNode()
         //self.layer2DHighlight = SKNode()
         self.cursorNode = SKSpriteNode(imageNamed: "Selection")
         self.cursorNode.zPosition = 40.0
         self.cursorPoint = GridPoint(x: 0, y: 0)
-        
+
         self.terrainTransitionManager = TerrainTransitionManager()
-        
+
         super.init(size: size)
-        self.anchorPoint = CGPoint(x:0.5, y:0.5)
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
     }
 
     override func didMove(to view: SKView) {
-        
+
         let deviceScale = self.size.width / 667
-        
-        self.terrainView.position = CGPoint(x:-self.size.width*0.5, y:-self.size.height*0.2)
+
+        self.terrainView.position = CGPoint(x: -self.size.width * 0.5, y: -self.size.height * 0.2)
         self.terrainView.xScale = deviceScale
         self.terrainView.yScale = deviceScale
         self.addChild(terrainView)
-        
+
         self.placeAllTiles2D()
-        
+
         self.cam = SKCameraNode() //initialize and assign an instance of SKCameraNode to the cam variable.
         self.cam.xScale = 1 // 0.25
         self.cam.yScale = 1 // 0.25 //the scale sets the zoom level of the camera on the given position
-        
+
         self.camera = cam //set the scene's camera to reference cam
         self.addChild(cam) //make the cam a childElement of the scene itself.
-        
+
         // position the camera on the gamescene.
         self.cam.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        
+
         // cursor
         self.cursorNode.anchorPoint = CGPoint(x: 0, y: 0)
         self.terrainView.addChild(self.cursorNode)
     }
-    
+
     func placeAllTiles2D() {
-        
+
         let width: Int = (self.grid?.width)!
         let height: Int = (self.grid?.height)!
-        
+
         for x in 0..<width {
-            
+
             for y in 0..<height {
-                
+
                 let gridPoint = GridPoint(x: x, y: y)
                 let point = self.grid?.screenPoint(from: gridPoint)
                 let tile = self.grid?.tile(at: gridPoint)
-                
+
                 place(tile: tile!, gridPoint: gridPoint, withPosition: point!)
             }
         }
     }
-    
+
     // render tile
-    // 
+    //
     // zlevel
     // 60 units
     // 50 features
@@ -118,12 +118,12 @@ class GameScene: SKScene {
     // 10 terrain
     //
     func place(tile: Tile, gridPoint: GridPoint, withPosition position: CGPoint) {
-        
+
         let terrain = tile.terrain
-        
+
         // terrain sprite
         self.terrainView.addChild(TerrainSpriteNode(withPosition: position, andTerrain: terrain!))
-        
+
         // terrain transitions
         let remoteNE = self.grid?.tile(at: gridPoint.neighbor(in: .northEast)).terrain!
         let remoteSE = self.grid?.tile(at: gridPoint.neighbor(in: .southEast)).terrain!
@@ -132,45 +132,45 @@ class GameScene: SKScene {
         let remoteNW = self.grid?.tile(at: gridPoint.neighbor(in: .northWest)).terrain!
         let remoteN = self.grid?.tile(at: gridPoint.neighbor(in: .north)).terrain!
         let remotePattern = "\((remoteNE?.rawValue)!),\((remoteSE?.rawValue)!),\((remoteS?.rawValue)!),\((remoteSW?.rawValue)!),\((remoteNW?.rawValue)!),\((remoteN?.rawValue)!)"
-        
+
         if let transitions = self.terrainTransitionManager.bestTransitions(forCenter: terrain!, remotePattern: remotePattern) {
-            
+
             for transition in transitions {
-            
+
                 self.terrainView.addChild(TransitionSpriteNode(withPosition: position, andTransition: transition))
             }
         }
-        
-        
+
+
         // grid
         self.terrainView.addChild(GridSpriteNode(withPosition: position))
-        
+
         // features
         for feature in tile.features {
-            
+
             self.terrainView.addChild(FeatureSpriteNode(withPosition: position, andFeature: feature))
         }
 
     }
-    
+
     func moveFocus(to gridpoint: GridPoint) {
 
         self.cursorNode.position = (self.grid?.screenPoint(from: gridpoint))!
     }
-    
+
     // handling touches
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+
         let touch = touches.first
         let touchLocation = touch?.location(in: self.terrainView)
-        
+
         let gridPoint = self.grid?.gridPoint(from: touchLocation!)
-        
+
         //NSLog("touch hex: \(gridPoint)")
-        
+
         // update the rendering
         self.moveFocus(to: gridPoint!)
-        
+
         // notify parent
         if self.cursorPoint == gridPoint {
             if let focusChanged = self.onFocusChanged {
@@ -179,10 +179,10 @@ class GameScene: SKScene {
         }
         self.cursorPoint = gridPoint
     }
-    
+
     // moving the map around
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+
         for touch in touches {
             let location = touch.location(in: self.terrainView)
             let previousLocation = touch.previousLocation(in: self.terrainView)
@@ -193,7 +193,7 @@ class GameScene: SKScene {
             self.cam.position.y -= deltaY * 0.5
         }
     }
-    
+
     override func update(_ currentTime: CFTimeInterval) {
         //NSLog("update")
     }
--- src/ViewControllers/MapViewController.swift
+++ src/ViewControllers/MapViewController.swift
@@ -9,66 +9,67 @@
 import UIKit
 
 class MapViewController: UIViewController, UIScrollViewDelegate {
-    
+
     @IBOutlet weak var scrollView: UIScrollView!
-    
+
     let slideImages: NSArray = ["MapTile01", "MapTile02", "MapTile03", "MapTile04", "MapTile05"]
     var kImageWidth: CGFloat = 0
     var kImageHeight: CGFloat = 0
     let kPadding: CGFloat = 0
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let bounds = UIScreen.main.bounds
         self.kImageWidth = bounds.size.width
         self.kImageHeight = self.kImageWidth // bounds.size.height
-        
+
         self.setupScrollView()
     }
-    
+
     //MARK: Set Infinite ScrollView
-    
+
     func setupScrollView() {
-        
+
         for i in 0..<slideImages.count {
             self.addBackgroundTile(withName: slideImages[i] as! String, atPosition: i)
         }
 
         self.scrollView.contentSize = CGSize(width: kImageWidth, height: kImageHeight * CGFloat(slideImages.count))
         //scrInfinite.scrollRectToVisible(CGRect(x: 0, y: 0, width: kImageWidth, height: kImageHeight), animated: false)
-        
+
         var mapsDict: NSDictionary?
         if let path = Bundle.main.path(forResource: "Maps", ofType: "plist") {
             mapsDict = NSDictionary(contentsOfFile: path)
         }
+
         if let mapsDict = mapsDict {
             let maps = mapsDict["Maps"] as! NSArray
-            
+
             for mapDict in maps {
                 let dict = mapDict as! NSDictionary
-                
+
                 //let title = dict["Title"] as! String
                 let x = self.kImageWidth * (dict["X"] as! CGFloat)
                 let y = self.kImageWidth * (dict["Y"] as! CGFloat)
                 let number = dict["Number"] as! NSNumber
-                
+
                 self.addButtonAt(x: x, y: y, andNumber: number.intValue)
             }
         }
     }
-    
+
     func addBackgroundTile(withName imageString: String, atPosition position: Int) {
-        
+
         let image = UIImage(named: imageString)!
         let imageView = UIImageView(image: image)
         imageView.frame = CGRect(x: 0, y: CGFloat(position) * (kImageHeight + kPadding), width: kImageWidth, height: kImageHeight)
         imageView.contentMode = .scaleToFill
         self.scrollView.addSubview(imageView)
     }
-    
+
     func addButtonAt(x: CGFloat, y: CGFloat, andNumber number: Int) {
-        
+
         let button = UIButton(type: UIButtonType.custom) as UIButton
         button.frame = CGRect(origin: CGPoint(x: x, y: y), size: CGSize(width: 50, height: 50))
         button.setBackgroundImage(UIImage(named: "Bubble"), for: .normal)
@@ -77,11 +78,11 @@ class MapViewController: UIViewController, UIScrollViewDelegate {
         button.addTarget(self, action: #selector(openGame), for: .touchUpInside)
         self.scrollView.addSubview(button)
     }
-    
+
     func openGame(sender: AnyObject) {
-        
+
         print("tag: \(sender.tag)")
-        
+
         let gameViewController = GameViewController.instantiateFromStoryboard("Main")
         //gameViewController.game = sender.tag!
         self.navigationController?.pushViewController(gameViewController, animated: true)
