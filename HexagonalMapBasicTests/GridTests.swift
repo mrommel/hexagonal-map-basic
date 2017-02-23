9c1d3efe72e18f1bc5f883ad95944b935b40094b
Michael Rommel <michael.rommel@mobility-media.de>
2017-02-23 14:51:06 +0100

    clean-up tests

--- HexagonalMapBasicTests/GridTests.swift
+++ HexagonalMapBasicTests/GridTests.swift
@@ -10,84 +10,99 @@ import XCTest
 
 class GridTests: XCTestCase {
     
-    var classUnderTest: Grid?
-    
-    override func setUp() {
-        super.setUp()
-        
-        self.classUnderTest = Grid(width: 5, height: 5)
-    }
-    
-    override func tearDown() {
-        
-        self.classUnderTest = nil
-        
-        super.tearDown()
-    }
-    
     func testDefaultInit() {
 
+        // Preconditions
+        let grid = Grid(width: 5, height: 5)
+        
         for x in 0..<5 {
             for y in 0..<5 {
-                let terrain = self.classUnderTest?.terrain(at: GridPoint(x: x, y: y))
+                // Stimulus
+                let terrain = grid?.terrain(at: GridPoint(x: x, y: y))
+                
+                // Assertion
                 XCTAssertEqual(terrain, Terrain.ocean, "terrain does not match")
             }
         }
     }
     
     func testOutside() {
-        let terrain = self.classUnderTest?.terrain(at: GridPoint(x: -1, y: -1))
+        
+        // Preconditions
+        let grid = Grid(width: 5, height: 5)
+        
+        // Stimulus
+        let terrain = grid?.terrain(at: GridPoint(x: -1, y: -1))
+        
+        // Assertion
         XCTAssertEqual(terrain, Terrain.outside, "terrain does not match")
     }
 
     func testTerrainTypePersisted() {
         
-        let terrain0 = Terrain.grass
-        self.classUnderTest?.set(terrain: terrain0, at: GridPoint(x: 2, y: 2))
+        // Preconditions
+        let grid = Grid(width: 5, height: 5)
         
-        let terrain1 = self.classUnderTest?.terrain(at: GridPoint(x: 2, y: 2))
-        XCTAssertEqual(terrain1, terrain0, "terrain types does not match")
+        // Stimulus
+        let terrainBefore = Terrain.grass
+        grid?.set(terrain: terrainBefore, at: GridPoint(x: 2, y: 2))
+        
+        // Assertion
+        let terrainAfter = grid?.terrain(at: GridPoint(x: 2, y: 2))
+        XCTAssertEqual(terrainBefore, terrainAfter, "terrain types does not match")
     }
     
     func testScreenPoint() {
         
-        let screenPoint = self.classUnderTest?.screenPoint(from: GridPoint(x: 2, y: 2))
+        // Preconditions
+        let grid = Grid(width: 5, height: 5)
+        
+        // Stimulus
+        let screenPoint = grid?.screenPoint(from: GridPoint(x: 2, y: 2))
+        
+        // Assertion
         XCTAssertEqual(screenPoint, CGPoint(x: 108, y: 124), "screen point does not match")
     }
     
     func testGridPoint() {
         
-        let gridPoint = self.classUnderTest?.gridPoint(from: CGPoint(x: 108, y: 124))
-        XCTAssertEqual(gridPoint, GridPoint(x: 2, y: 2), "grid point does not match")
+        // Preconditions
+        let grid = Grid(width: 5, height: 5)
         
+        // Stimulus
+        let gridPoint0 = grid?.gridPoint(from: CGPoint(x: 108, y: 124))
+        let gridPoint1 = grid?.gridPoint(from: CGPoint(x: 0, y: 0))
         
-        let gridPoint1 = self.classUnderTest?.gridPoint(from: CGPoint(x: 0, y: 0))
+        // Assertion
+        XCTAssertEqual(gridPoint0, GridPoint(x: 2, y: 2), "grid point does not match")
         XCTAssertEqual(gridPoint1, GridPoint(x: 0, y: 0), "grid point does not match")
     }
 
     func testAddFeatureSucceeds() {
     
         // Preconditions
+        let grid = Grid(width: 5, height: 5)
         
         // Stimulus
-        self.classUnderTest?.add(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
+        grid?.add(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
         
         // Assertion
-        let hasHill = self.classUnderTest?.has(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
+        let hasHill = grid?.has(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
         XCTAssertEqual(hasHill, true, "feature should have been added")
     }
     
     func testRemoveFeatureSucceeds() {
         
         // Preconditions
-        self.classUnderTest?.add(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
-        let hasHillBefore = self.classUnderTest?.has(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
+        let grid = Grid(width: 5, height: 5)
+        grid?.add(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
+        let hasHillBefore = grid?.has(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
         
         // Stimulus
-        self.classUnderTest?.remove(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
+        grid?.remove(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
         
         // Assertion
-        let hasHillAfter = self.classUnderTest?.has(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
+        let hasHillAfter = grid?.has(feature: Feature.hill, at: GridPoint(x: 2, y: 2))
         XCTAssertEqual(hasHillBefore, true, "feature should have been added")
         XCTAssertEqual(hasHillAfter, false, "feature should have been removed")
     }
